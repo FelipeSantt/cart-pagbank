3556a9ca234676f94fa856ddb8ab05eab8f2fc23
Felipe Santos <felipek2267@gmail.com>
2024-04-25 00:09:29 -0300

    Ajustando tela

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import breakpoints from '../styles/breakPoints';
+
+const HeaderPag = styled.div `
+    @media ${breakpoints.bg} {
+        img {
+           margin-left:0;
+        }
+    }
+
+`
 
 const Header = () => {
     return (
-        <header className='flex w-full h-3rem align-items-center px-8 border-bottom-1 border-green-500 ' >
-            <div className='logo'>
-                <img className='w-7 -ml-6 px-8' src={"https://logodownload.org/wp-content/uploads/2019/09/pagbank-logo-4-1.png"}/>
-            </div>
-        </header>
+        <HeaderPag>
+            <header className='flex w-full h-3rem align-items-center px-8 border-bottom-1 border-green-500 ' >
+                <div className='logo'>
+                    <img className= 'ml-5' src={"https://assets.pagseguro.com.br/ps-bootstrap/v6.90.17/svg/pagbank/logo-pagbank.svg"}/>
+                </div>
+            </header>
+        </HeaderPag>
     );
 };
 
